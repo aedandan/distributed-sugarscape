eedd46e2891423f6949b98d3b2804eede58287a3
Aniqua Tabassum <aniquaTabassum@ap21.uc.osg-htc.org>
2024-03-06 22:45:05 -0600

    Made adding batch type mandatory

--- WQ_Factory/WorkQueueFactory.py
+++ WQ_Factory/WorkQueueFactory.py
@@ -1,9 +1,18 @@
 import subprocess
 import sys
 
+condor_batch_type = "condor"
+slurm_batch_type = "slurm"
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python3 WorkQueueFactory.py manager_name")
+    if len(sys.argv) != 3:
+        print("Usage: python3 WorkQueueFactory.py manager_name batch_type")
         sys.exit(1)
     manager_name = sys.argv[1]
-    subprocess.run(["work_queue_factory", "-T", "condor", "-M", manager_name])
+    batch_type = sys.argv[2]
+
+    if batch_type == slurm_batch_type:
+        subprocess.run(["work_queue_factory", "-T", batch_type, "-B", "--partition=skx -t 0:20:00", "-M", manager_name])
+    elif batch_type == condor_batch_type:
+        subprocess.run(["work_queue_factory", "-T", batch_type, "-M", manager_name])
+    else:
+        print(f"Batch type {batch_type} not supported yet. Please choose between condor and slurm")
