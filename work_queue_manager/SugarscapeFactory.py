4a77ec53e82ec9cdc6aea24acf10ff25d25ca407
amccall <mccallaedan@gmail.com>
2024-03-07 18:29:03 -0600

    Refactor SugarscapeFactory.py to time simulations run on through Work Queue

--- work_queue_manager/SugarscapeFactory.py
+++ work_queue_manager/SugarscapeFactory.py
@@ -3,6 +3,8 @@ import os
 import sys
 import signal
 import re
+import time
+import getopt
 from typing import List
 from typing import Dict
 
@@ -25,8 +27,9 @@ def interruptHandler(signal, frame):
     sys.exit(0)
 
 # Function that times runs of a decision model based on the list of seeds passed in
-def timeSeeds(seeds: List[str], WorkQueue: WorkQueue):
-    for file in seeds_trimmed :
+def timeSeeds(seeds: List[str], WorkQueue: WorkQueue) -> float:
+    jobs_start = time.time()
+    for file in seeds:
         command = f"python3 sugarscape.py --conf {file}.config > {file}.json"
         task = WorkQueue.Task(command)
         task.specify_input_file("../sugarscape.py", "sugarscape.py", cache = True)
@@ -40,7 +43,7 @@ def timeSeeds(seeds: List[str], WorkQueue: WorkQueue):
         print(f"Task submitted: python3 sugarscape.py --conf {file}.config\n")
         WorkQueue.submit(task)
 
-    while not WorkQueue.empty() :
+    while not WorkQueue.empty():
         t = WorkQueue.wait()
         if t:
             print("Task {} has returned!".format(t.id))
@@ -52,13 +55,46 @@ def timeSeeds(seeds: List[str], WorkQueue: WorkQueue):
             else:
                 print("There was a problem executing the task.")
         else:
-            print("nada")
+            print("No task generated.")
+    jobs_end = time.time() - jobs_start
+    return jobs_end
 
-seeds_list = getSeeds()
-print(seeds_list)
+def readCommandLineArguments() -> int:
+    commandLineArgs = sys.argv[1:]
+    short_options = 's:'
+    long_options = "--seeds"
+    try:
+        arguments, values = getopt.getopt(commandLineArgs, shortopts=short_options, longopts=long_options)
+    except getopt.GetoptError as error:
+        print(error)
+        printUsage()
+    if len(arguments) != 1 or arguments[0][0] not in("-s", "--seeds"):
+        printUsage()
+    print(arguments)
+    return arguments[0][1]
 
+def printUsage():
+    print("USAGE: python3 SugarscapeFactory.py -s [number of seeds per decision model]")
+    sys.exit(0)
 
-# try:
-#     q = wq.WorkQueue(name="sugarscape", debug_log = "my.debug.log", transactions_log="my.transaction.log", port=9125)
-# except Exception :
-#     print("Could not activate Work Queue: ", sys.exception())
+if __name__ == "__main__":
+    signal.signal(signal.SIGINT, interruptHandler)
+    try:
+        q = WorkQueue.WorkQueue(name="sugarscape", debug_log = "debug.sugarscape.distrib.log", transactions_log="transaction.sugarscape.distrib.log", port=9125)
+    except Exception :
+        print("Could not activate Work Queue: ", sys.exception())
+
+    all_sugarscape_seeds = getSeeds()
+    number_of_seeds = readCommandLineArguments()
+    print(number_of_seeds)
+    decision_model_times = {}
+    total_decision_model_time = 0
+    for decision_model in all_sugarscape_seeds:
+        decision_model_simulation_duration = timeSeeds(all_sugarscape_seeds[decision_model])
+        decision_model_times[decision_model] = decision_model_simulation_duration
+        total_decision_model_time += decision_model_simulation_duration
+        average_duration_per_simulation = decision_model_simulation_duration / number_of_seeds
+        print("{} took {} seconds to run all simulations, with an average of {} per simulation"
+              .format(decision_model, decision_model_simulation_duration, average_duration_per_simulation))
+    sys.exit(0)
+    
