2139643abea21a800173fe9caa19d53719e18ef7
Aniqua Tabassum <aniquaTabassum@ap21.uc.osg-htc.org>
2024-03-07 19:44:07 -0600

    Added min and max worker

--- WQ_Factory/WorkQueueFactory.py
+++ WQ_Factory/WorkQueueFactory.py
@@ -11,8 +11,8 @@ if __name__ == "__main__":
     batch_type = sys.argv[2]
 
     if batch_type == slurm_batch_type:
-        subprocess.run(["work_queue_factory", "-T", batch_type, "-B", "--partition=skx -t 0:20:00", "-M", manager_name])
+        subprocess.run(["work_queue_factory", "-T", batch_type, "-B", "--partition=skx -t 0:20:00", "-M", manager_name, "-w", "10", "-W", "50", "--workers-per-cycle", "10"])
     elif batch_type == condor_batch_type:
-        subprocess.run(["work_queue_factory", "-T", batch_type, "-M", manager_name])
+        subprocess.run(["work_queue_factory", "-T", batch_type, "-M", manager_name, "-w", "10", "-W", "50", "--workers-per-cycle", "10"])
     else:
         print(f"Batch type {batch_type} not supported yet. Please choose between condor and slurm")
